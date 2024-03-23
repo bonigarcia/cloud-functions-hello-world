657ceeb9690d0e7dbbc6e424d7cdcefeb9e59734
Boni Garcia <boni.garcia@uc3m.es>
2024-03-23 13:08:01 +0100

    Include GET /add-message endpoint

--- functions/index.js
+++ functions/index.js
@@ -6,10 +6,34 @@ admin.initializeApp();
 
 const logger = functions.logger;
 const app = express();
+const db = admin.firestore();
 
+// Hello world endpoint (GET)
 app.get("/hello-world", (req, res) => {
     logger.log("Hello world received");
     return res.status(200).send("Hello world!");
+});
+
+// Create message on Firestore (GET)
+app.get("/add-message", async (req, res) => {
+    try {
+        let message = req.query.message;
+        logger.log("message " + message);
+        if (message === undefined) {
+            return res.status(400).send("Missing query parameter. Use ?message=something");
+        }
+        const result = await db
+            .collection("messages")
+            .add({
+                message: message
+            });
+        return res.status(200).json({
+            result: `Added message with id ${result.id}`
+        });
+    } catch (error) {
+        logger.error("Error " + error);
+        return res.status(500).send(error);
+    }
 });
 
 exports.app = functions.https.onRequest(app);
